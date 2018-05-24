9b3cc5cd94bb46f7e60e34bbe0ad20793324d429
Brandon Ellis <thebellis618@gmail.com>
2018-05-23 22:44:49 -0400

    Now marks and saves posts from both QT chats and Pokebeach forum posts

--- beachMarkup.js
+++ beachMarkup.js
@@ -1,5 +1,70 @@
+chrome.storage.local.get(function(result){console.log(result)})
+
+function parseDate(dateString){
+	let atPos = dateString.indexOf("at");
+	let date = dateString.substring(0, atPos);
+	let time = dateString.substring(atPos + 2);
+	return Date.parse(date + time) / 1000;
+}
+
+function packagePost(item){
+	var node = item.cloneNode(true);
+	
+	let user = node.getAttribute("data-author");
+	let date = 123;
+	let html = undefined;
+	
+	let pc = node.getElementsByClassName("primaryContent")[0];
+	if (pc){
+		let editDate = pc.getElementsByClassName("editDate")[0];
+		let signature = pc.getElementsByClassName("signature")[0];
+		let messageMeta = pc.getElementsByClassName("messageMeta")[0];
+		
+		//Set date
+		if(messageMeta) {
+//			console.log("message found");
+			let privateControls = messageMeta.getElementsByClassName("privateControls")[0];
+			if (privateControls){
+//				console.log("privateControls found");
+				let mutedItem = privateControls.getElementsByClassName("muted")[0];
+				if (mutedItem){
+//					console.log("mutedItem found");
+					let datePermalink = mutedItem.getElementsByClassName("datePermalink")[0];
+					if (datePermalink){
+//						console.log("datePermalink found");
+						let dateTime = datePermalink.getElementsByClassName("DateTime")[0];
+						if (dateTime){
+//							console.log("dateTime found");
+							let dateString = dateTime.getAttribute("title");// + 
+											 //dateTime.getAttribute("data-timestring");
+							date = parseDate(dateString);
+						}
+					}
+				}
+			}
+		}
+		
+		if(editDate) pc.removeChild(editDate);
+		if(signature) pc.removeChild(signature);
+		if(messageMeta) pc.removeChild(messageMeta);
+	}
+	
+	html = node.innerHTML;
+
+	return {
+		user : user,
+		date : date,
+		html : html,
+		type : "pokebeach"
+	};
+}
+
 let x = Array.from(document.getElementsByClassName("message"));
+
+
 x.forEach(function (item) {
+	let id = item.id;
+
 	//create Checkbox
 	let check = document.createElement("INPUT");
 	check.setAttribute("type", "checkbox");
@@ -13,15 +78,28 @@ x.forEach(function (item) {
 	label2.classList.add("important-label");
 	label2.classList.add("item");
 	label2.appendChild(check);
-
+	
+	//Mark as important if id is already in the storage
+	chrome.storage.local.get(id, function(data){
+		if (typeof data[id] !== 'undefined') {
+			item.classList.add("important");
+			check.checked = true;
+		}
+	});
 	
 	//Checkbox functionality (Will be set once added)
 	function onChange(box){
 		if (box.checked){
 			item.classList.add("important");
+			chrome.storage.local.set({[id]: packagePost(item)}, function() {
+		          console.log(id + ' added to storage!');
+		    });
 		}
 		else{
 			item.classList.remove("important");
+			chrome.storage.local.remove([id], function() {
+		          console.log(id + ' removed from storage!');
+		    });
 		}
 	};
 	
---
+++ qtMarkup.js
@@ -0,0 +1,124 @@
+chrome.storage.local.get(function(result){console.log(result)})
+
+function parseDate(dateString, timeString){
+	let pmPos = timeString.indexOf("PM");
+	let amPos = timeString.indexOf("AM");
+	let atPos = undefined;
+	if (pmPos != -1)
+		atPos = pmPos;
+	else
+		atPos = amPos;
+	
+	timeString = timeString.substring(0, atPos+2);
+	let tempDate = new Date(dateString + " " + timeString);
+	console.log(tempDate);
+	
+	return tempDate.getTime() / 1000;
+}
+
+function packagePost(item){
+	var node = item.cloneNode(true);
+	
+	let user = undefined;
+	let date = undefined;
+	let html = undefined;
+
+	let userNode = node.getElementsByClassName("messageauthor");
+	if (userNode) {
+		userNode = userNode[0];
+		userNode = userNode.childNodes[0];
+		user = userNode.data;
+	}
+	
+	let pc = node.getElementsByClassName("topic-messageauthorcontainer");
+	if (pc) {
+		pc = pc[0];
+		let dateNode = pc.getElementsByClassName("messagedata")[0];
+		let timeNode = pc.getElementsByClassName("messagedata")[1];
+		date = parseDate(dateNode.innerHTML, timeNode.innerHTML);
+	}
+	
+	//Strip checkbox from post
+	if (pc){
+		let messageMeta = pc.getElementsByClassName("messageMeta");
+		if (messageMeta) messageMeta = messageMeta[0];
+		
+		if(messageMeta) pc.removeChild(messageMeta);
+	}
+	
+	html = node.innerHTML;
+
+	return {
+		user : user,
+		date : date,
+		html : html,
+		type : "qt"
+	};
+}
+
+let x = Array.from(document.getElementsByClassName("messagerow"));
+
+
+x.forEach(function (item) {
+	//Get ID
+	let id = undefined;
+	let messageNumber = item.getElementsByClassName("messagenumber")[0];
+	id = messageNumber.childNodes[0].getAttribute("name");
+	
+	let pathArray = window.location.pathname.split( '/' );
+	let chatNumber = pathArray.slice(-1)[0];
+	
+	id += "-" + chatNumber;
+
+	//create Checkbox
+	let check = document.createElement("INPUT");
+	check.setAttribute("type", "checkbox");
+	check.classList.add("important-checkbox");
+	
+	let label = document.createElement("span");
+	label.classList.add("item");
+	label.appendChild(document.createTextNode("Important: "));
+	
+	let label2 = document.createElement("span");
+	label2.classList.add("important-label");
+	label2.classList.add("item");
+	label2.appendChild(check);
+	
+	let div = document.createElement("DIV");
+	div.classList.add("messageMeta");
+	div.appendChild(label);
+	div.appendChild(label2);
+	
+	//Mark as important if id is already in the storage
+	chrome.storage.local.get(id, function(data){
+		if (typeof data[id] !== 'undefined') {
+			item.classList.add("important");
+			check.checked = true;
+		}
+	});
+	
+	//Checkbox functionality (Will be set once added)
+	function onChange(box){
+		if (box.checked){
+			item.classList.add("important");
+			chrome.storage.local.set({[id]: packagePost(item)}, function() {
+		          console.log(id + ' added to storage!');
+		    });
+		}
+		else{
+			item.classList.remove("important");
+			chrome.storage.local.remove([id], function() {
+		          console.log(id + ' removed from storage!');
+		    });
+		}
+	};
+	
+	let messagecell = item.getElementsByClassName("messagecell")[0];
+	if (messagecell){
+		messagecell.appendChild(div);
+		messagecell.getElementsByClassName("important-label")[0]
+			.getElementsByClassName("important-checkbox")[0]
+			.onchange = function() {onChange(this);};
+	}
+		
+})
